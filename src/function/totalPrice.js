0c9595ba686b15437ad2d5066d40f269dbfdacb5
firmanssssss <avenged.firman33@gmail.com>
2023-08-09 02:36:32 +0700

    debuggging

--- src/function/totalPrice.js
+++
@@ -1,9 +0,0 @@
-const cart = JSON.parse(localStorage.getItem("@cart"));
-
-export const total = () => {
-  let totalPrice = 0;
-  for (const item of cart) {
-    totalPrice += item.price;
-  }
-  return totalPrice;
-};
