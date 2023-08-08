425da693eb6a390ac8b2e8570349ab5826ac93b9
firmanssssss <avenged.firman33@gmail.com>
2023-08-08 20:26:03 +0700

    add: favorite icon and some tooltip in this icon and cart icon

--- src/components/Fragments/Navbar/isLogin.jsx
+++ src/components/Fragments/Navbar/isLogin.jsx
@@ -8,15 +8,24 @@ export default function IsLogin() {
   console.log(user);
   return (
     <div className="flex justify-evenly items-center w-full">
-      <div className="dropdown dropdown-end">
-        <label tabIndex={0} className="btn btn-ghost btn-circle">
+      <div className="tooltip tooltip-bottom" data-tip="Favorite">
+        <a className="btn btn-ghost btn-circle rounded-full">
           <div className="indicator">
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
-            </svg>
-            <span className="badge badge-sm indicator-item">0</span>
+            <img src="/public/images/png/heart.png" className="h-5 w-5" />
           </div>
-        </label>
+        </a>
+      </div>
+      <div className="dropdown dropdown-end">
+        <div className="tooltip tooltip-bottom" data-tip="keranjang">
+          <label tabIndex={0} className="btn btn-ghost btn-circle">
+            <div className="indicator">
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
+              </svg>
+              <span className="badge badge-sm indicator-item">0</span>
+            </div>
+          </label>
+        </div>
         <div tabIndex={0} className="mt-3 z-[1] card card-compact dropdown-content w-52 bg-base-100 shadow">
           <div className="card-body">
             <span className="font-bold text-lg">0 Items</span>
